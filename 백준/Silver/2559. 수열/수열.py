604db15b2176762f0fafb99177d1b640c846dd62
Arne919 <ghgghg96@naver.com>
2025-07-09 20:05:29 +0900

    [Silver III] Title: 수열, Time: 64 ms, Memory: 40156 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+
+result = []
+result.append(sum(arr[:K]))
+
+for i in range(N-K):
+    result.append(result[i] - arr[i] + arr[K+i])
+
+print(max(result))
