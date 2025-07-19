70f5a70da1b2cbc653b86b1082bece446658a1c0
Arne919 <ghgghg96@naver.com>
2025-07-19 21:55:45 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2608 ms, Memory: 42656 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,12 @@
+n = int(input())
+company = set()
+
+for _ in range(n):
+    name, action = input().split()
+    if action == 'enter':
+        company.add(name)
+    else:  # action == 'leave'
+        company.discard(name)
+
+for name in sorted(company, reverse=True):
+    print(name)
