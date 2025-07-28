8968811a00b04c7e4af8d2fa1c9222985a7096a3
Arne919 <ghgghg96@naver.com>
2025-07-28 21:55:20 +0900

    [Silver III] Title: 분수 합, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -0,0 +1,10 @@
+import math
+
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+numerator = a * d + c * b
+denominator = b * d
+
+gcd = math.gcd(numerator, denominator)
+print(numerator // gcd, denominator // gcd)
