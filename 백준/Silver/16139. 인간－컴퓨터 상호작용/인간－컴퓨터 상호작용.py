f60c97d5a04b344eb3036d6e4fcfb819081f6fdf
Arne919 <ghgghg96@naver.com>
2025-07-29 23:21:55 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 808 ms, Memory: 110236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 S = input().strip()
 q = int(input())
 
-prefix = [[0] * (len(S) + 1) for _ in range(26)]
+# count[i] = S[0:i]까지 각 알파벳 누적 개수를 담은 리스트
+count = {0: [0] * 26}
 
-for i in range(len(S)):
-    c = ord(S[i]) - ord('a')
-    for j in range(26):
-        prefix[j][i+1] = prefix[j][i] + (1 if j == c else 0)
+for i, ch in enumerate(S):
+    count[i + 1] = count[i][:]  # 리스트 복사
+    count[i + 1][ord(ch) - ord('a')] += 1
 
 for _ in range(q):
     ch, l, r = input().split()
     l = int(l)
     r = int(r)
-    c = ord(ch) - ord('a')
-    print(prefix[c][r+1] - prefix[c][l])
+    idx = ord(ch) - ord('a')
+    print(count[r + 1][idx] - count[l][idx])
