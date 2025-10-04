88caebce4833f4c4277738155c55a86cef6b27cd
Arne919 <ghgghg96@naver.com>
2025-10-04 10:07:40 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,12 @@
+T = int(input().strip())
+
+for _ in range(T):
+    C = int(input().strip())
+    quarters = C // 25
+    C %= 25
+    dimes = C // 10
+    C %= 10
+    nickels = C // 5
+    C %= 5
+    pennies = C
+    print(quarters, dimes, nickels, pennies)
