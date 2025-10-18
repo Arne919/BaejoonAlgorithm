9d34d15d22bdcef136072eec36ef2426046b694f
Arne919 <ghgghg96@naver.com>
2025-10-19 01:40:18 +0900

    [Silver II] Title: 에디터, Time: 240 ms, Memory: 42832 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+left_stack = list(input().strip())  # 초기 문자열
+right_stack = []
+
+m = int(input())  # 명령 개수
+
+for _ in range(m):
+    cmd = input().split()
+    if cmd[0] == 'L':
+        if left_stack:
+            right_stack.append(left_stack.pop())
+    elif cmd[0] == 'D':
+        if right_stack:
+            left_stack.append(right_stack.pop())
+    elif cmd[0] == 'B':
+        if left_stack:
+            left_stack.pop()
+    elif cmd[0] == 'P':
+        left_stack.append(cmd[1])
+
+print(''.join(left_stack + right_stack[::-1]))
