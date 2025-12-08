ae27a478a39a0124db22908da816dda33ddd7d79
Arne919 <ghgghg96@naver.com>
2025-12-08 22:04:14 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+# 0~20 까지 저장하는 DP 테이블
+dp = [[[None] * 21 for _ in range(21)] for __ in range(21)]
+
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+
+    if dp[a][b][c] is not None:
+        return dp[a][b][c]
+
+    if a < b and b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+    else:
+        dp[a][b][c] = (w(a-1, b, c) 
+                       + w(a-1, b-1, c) 
+                       + w(a-1, b, c-1) 
+                       - w(a-1, b-1, c-1))
+    return dp[a][b][c]
+
+
+# 입력 처리
+for line in sys.stdin:
+    a, b, c = map(int, line.split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    print(f"w({a}, {b}, {c}) = {w(a, b, c)}")
